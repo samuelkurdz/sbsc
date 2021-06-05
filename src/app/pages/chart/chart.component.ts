eff410df35379e1b6564d20ae5b4b598b52ad75c
Samuel Adebayo <kurdzsamuel@gmail.com>
2021-06-05 16:29:35 +0100

    chart added

--- src/app/pages/chart/chart.component.ts
+++ src/app/pages/chart/chart.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ChartOptions, ChartType } from 'chart.js';
+import { Colors, Label, SingleDataSet } from 'ng2-charts';
 import { ChartData } from 'src/app/models/general.interface';
 
 @Component({
@@ -17,6 +19,37 @@ export class ChartComponent implements OnInit {
   @Input() totalColor: string = '';
   @Input() balanceColor: string = '';
 
+    // Doughnut
+    options: ChartOptions = {
+      cutoutPercentage: 80,
+      legend: {
+        display: false,
+      },
+      tooltips: {
+        displayColors: false,
+      },
+      elements: {
+        arc: {
+          borderWidth: 0,
+        }
+      },
+      // title: {
+      //   text: 'knslknegklnlknlkgn',
+      //   display: true,
+      // }
+    };
+    chartColors: Array<Colors> = [
+      {
+        backgroundColor: ["#FF6542", '#F8E9E2'],
+      },
+    ];
+    doughnutChartLabels: Label[] = [
+      'Spent',
+      'Balance',
+    ];
+    doughnutChartData: SingleDataSet = [60, 40];
+    doughnutChartType: ChartType = 'doughnut';
+  
   constructor() { }
 
   ngOnInit(): void {
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { SvgComponent } from './svg/svg.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ChartsModule } from 'ng2-charts';
 
 
 
@@ -14,13 +15,15 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     CommonModule,
     HttpClientModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    ChartsModule
   ],
   exports: [
     SvgComponent,
     HttpClientModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    ChartsModule
   ]
 })
 export class SharedModule { }
